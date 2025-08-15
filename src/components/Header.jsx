97bf6c81d2b0987415716940689cb9c352bfb7d2
Neharaj08 <kaveryrg258@gmail.com>
2025-08-15 15:43:07 +0530

    Update dashboard sidebar styling and layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
 
-function Header() {
+import React from 'react';
+import { FaBell } from 'react-icons/fa';
+import { FiSearch } from 'react-icons/fi';
+
+export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="app-header">
+      <div></div>
+      <div className="search-wrap">
+        <FiSearch className="search-icon" />
+        <input type="text" placeholder="Search here..." className="search-input" />
+      </div>
+      <div className="header-right">
+        <div className="bell-wrap">
+          <FaBell className="bell-icon" />
+          <span className="bell-dot"></span>
+        </div>
+        <div className="profile">
+          <img
+            src="https://randomuser.me/api/portraits/women/65.jpg"
+            alt="User"
+            className="avatar"
+          />
+          <span className="name">Sufy</span>
+        </div>
+      </div>
+    </header>
+  );
 }
-
-export default Header
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FiHome, FiUsers, FiBox, FiSettings, FiHelpCircle, FiUser } from 'react-icons/fi';
+import hanger from '../assets/Screenshot 2025-08-14 222650.png';
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="sidebar">
+      <div className="logo">
+        <img src={hanger} alt="StarFashion logo" />
+        <span>StarFashion</span>
+      </div>
+      <ul className="nav-list">
+        <li>
+          <NavLink to="/" end>
+            <FiHome className="icon" /> Dashboard
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/customers">
+            <FiUsers className="icon" /> Customers
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/orders">
+            <FiBox className="icon" /> Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/vendors">
+            <FiUser className="icon" /> Vendors
+          </NavLink>
+        </li>
+      </ul>
+
+      <div className="nav-section-label">OTHERS</div>
+      <ul className="nav-list">
+        <li>
+          <NavLink to="/settings">
+            <FiSettings className="icon" /> Settings
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/account">
+            <FiUser className="icon" /> Account
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/help">
+            <FiHelpCircle className="icon" /> Help
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
